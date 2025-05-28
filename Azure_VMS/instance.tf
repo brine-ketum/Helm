3edb5ccf1f1aba96c7834b9462ae44558a2b7b5b
brine-ketum <brinendamketum@gmail.com>
2025-05-28 10:47:56 -0400

    Add WIndows, RHEL and CentOS

--- Azure_VMS/instance.tf
+++ Azure_VMS/instance.tf
@@ -1,11 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
 
-# Provider Block
 provider "azurerm" {
   features {}
   subscription_id = "16e5b426-6372-4975-908a-e4cc44ee3cab"
 }
 
-# Resource Group - Renamed to BrineK
+# Resource Group
 resource "azurerm_resource_group" "brinek_rg" {
   name     = "BrineK"
   location = "EastUS2"
@@ -27,25 +34,7 @@ resource "azurerm_subnet" "brinek_public_subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Public IP for Ubuntu VM
-resource "azurerm_public_ip" "ubuntu_public_ip" {
-  name                = "UbuntuPublicIP"
-  location            = azurerm_resource_group.brinek_rg.location
-  resource_group_name = azurerm_resource_group.brinek_rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-# Public IP for RHEL VM
-# resource "azurerm_public_ip" "rhel_public_ip" {
-#   name                = "RHELPublicIP"
-#   location            = azurerm_resource_group.brinek_rg.location
-#   resource_group_name = azurerm_resource_group.brinek_rg.name
-#   allocation_method   = "Static"
-#   sku                 = "Standard"
-# }
-
-# Network Security Group (NSG)
+# NSG
 resource "azurerm_network_security_group" "brinek_nsg" {
   name                = "BrinekNSG"
   location            = azurerm_resource_group.brinek_rg.location
@@ -63,43 +52,6 @@ resource "azurerm_network_security_group" "brinek_nsg" {
     destination_address_prefix = "*"
   }
 
-# Allow HTTP from your IP
-  security_rule {
-    name                       = "AllowHTTPFromMyIP"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "40.143.44.44/32"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "AllowHTTPToBitbucket"
-    priority                   = 140
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "7990"
-    source_address_prefix      = "40.143.44.44/32"
-    destination_address_prefix = "*"
-  }
-
-# For Terraform NSG
-security_rule {
-  name                       = "AllowPgAdmin"
-  priority                   = 130
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "Tcp"
-  source_port_range          = "*"
-  destination_port_range     = "5050"
-  source_address_prefix      = "40.143.44.44/32"
-  destination_address_prefix = "*"
-}
-
   security_rule {
     name                       = "AllowAllOutbound"
     priority                   = 200
@@ -111,17 +63,50 @@ security_rule {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+    security_rule {
+    name                       = "AllowRDPInbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"  # Change to your actual IP or CIDR
+    destination_address_prefix = "*"
+  }
+
+security_rule {
+  name                       = "AllowWinRMInbound"
+  priority                   = 115
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_ranges    = ["5985", "5986"]
+  source_address_prefix      = "*"  # or restrict to control server IP
+  destination_address_prefix = "*"
+}
 }
 
-# NSG Association with Subnet
 resource "azurerm_subnet_network_security_group_association" "brinek_nsg_assoc" {
   subnet_id                 = azurerm_subnet.brinek_public_subnet.id
   network_security_group_id = azurerm_network_security_group.brinek_nsg.id
 }
 
-# # Network Interface for Ubuntu VM
+# Loop for 5 VMs
+resource "azurerm_public_ip" "ubuntu_pip" {
+  count               = 1
+  name                = "UbuntuPublicIP-${count.index}"
+  location            = azurerm_resource_group.brinek_rg.location
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_network_interface" "ubuntu_nic" {
-  name                = "UbuntuNIC"
+  count               = 1
+  name                = "UbuntuNIC-${count.index}"
   location            = azurerm_resource_group.brinek_rg.location
   resource_group_name = azurerm_resource_group.brinek_rg.name
 
@@ -129,74 +114,20 @@ resource "azurerm_network_interface" "ubuntu_nic" {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.brinek_public_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.ubuntu_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.ubuntu_pip[count.index].id
   }
 }
 
-
-resource "azurerm_public_ip" "ubuntu_lb_pip" {
-  name                = "UbuntuLBFIP"
-  location            = azurerm_resource_group.brinek_rg.location
-  resource_group_name = azurerm_resource_group.brinek_rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_lb" "ubuntu_lb" {
-  name                = "UbuntuLB"
-  location            = azurerm_resource_group.brinek_rg.location
-  resource_group_name = azurerm_resource_group.brinek_rg.name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                 = "FrontEnd"
-    public_ip_address_id = azurerm_public_ip.ubuntu_lb_pip.id
-  }
-}
-
-
-resource "azurerm_lb_nat_rule" "ssh_ubuntu" {
-  name                           = "SSHUbuntu"
-  resource_group_name            = azurerm_resource_group.brinek_rg.name
-  loadbalancer_id                = azurerm_lb.ubuntu_lb.id
-  protocol                       = "Tcp"
-  frontend_port                  = 60022
-  backend_port                   = 22
-  frontend_ip_configuration_name = "FrontEnd"
-}
-
-
-resource "azurerm_network_interface_nat_rule_association" "ubuntu_nat" {
-  network_interface_id  = azurerm_network_interface.ubuntu_nic.id
-  ip_configuration_name = "ipconfig1"
-  nat_rule_id           = azurerm_lb_nat_rule.ssh_ubuntu.id
-}
-
-# Network Interface for RHEL VM
-# resource "azurerm_network_interface" "rhel_nic" {
-#   name                = "RHELNIC"
-#   location            = azurerm_resource_group.brinek_rg.location
-#   resource_group_name = azurerm_resource_group.brinek_rg.name
-
-#   ip_configuration {
-#     name                          = "ipconfig1"
-#     subnet_id                     = azurerm_subnet.brinek_public_subnet.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = azurerm_public_ip.rhel_public_ip.id
-#   }
-# }
-
-# # Ubuntu Linux VM
 resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
-  name                = "UbuntuVM"
-  resource_group_name = azurerm_resource_group.brinek_rg.name
-  location            = azurerm_resource_group.brinek_rg.location
-  size                = "Standard_D4s_v3"
-  admin_username      = "brine"
-  admin_password      = "Bravedemo123!"
-  network_interface_ids = [
-    azurerm_network_interface.ubuntu_nic.id,
-  ]
+  count                 = 1
+  name                  = "UbuntuVM-${count.index}"
+  resource_group_name   = azurerm_resource_group.brinek_rg.name
+  location              = azurerm_resource_group.brinek_rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "brine"
+  disable_password_authentication = true
+
+  network_interface_ids = [azurerm_network_interface.ubuntu_nic[count.index].id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -211,97 +142,297 @@ resource "azurerm_linux_virtual_machine" "ubuntu_vm" {
     version   = "latest"
   }
 
+  admin_ssh_key {
+    username   = "brine"
+    public_key = file("/Users/brinketu/Downloads/eks-terraform-key.pub")
+  }
+
   custom_data = base64encode(<<-EOF
     #!/bin/bash
     sudo apt update
-    sudo apt-get install -y nginx
+    sudo apt install -y nginx
   EOF
   )
 
-  disable_password_authentication = false
-
   tags = {
-    Name = "UbuntuVM"
+    Name = "UbuntuVM-${count.index}"
     Env  = "Development"
   }
 }
 
-
-resource "azurerm_lb_nat_rule" "bitbucket_ui" {
-  name                           = "BitbucketUI"
-  resource_group_name            = azurerm_resource_group.brinek_rg.name
-  loadbalancer_id                = azurerm_lb.ubuntu_lb.id
-  protocol                       = "Tcp"
-  frontend_port                  = 60090
-  backend_port                   = 7990
-  frontend_ip_configuration_name = "FrontEnd"
+output "public_ips" {
+  value = [for pip in azurerm_public_ip.ubuntu_pip : pip.ip_address]
 }
 
-resource "azurerm_network_interface_nat_rule_association" "bitbucket_ui_assoc" {
-  network_interface_id  = azurerm_network_interface.ubuntu_nic.id
-  ip_configuration_name = "ipconfig1"
-  nat_rule_id           = azurerm_lb_nat_rule.bitbucket_ui.id
+output "ssh_instructions" {
+  value = [for i in range(1) : "ssh -i eks-terraform-key.pem brine@${azurerm_public_ip.ubuntu_pip[i].ip_address}"]
 }
+
+output "ansible_inventory" {
+  value = join("\n", concat(
+    ["[azure_vms]"],
+    [for i, pip in azurerm_public_ip.ubuntu_pip :
+      "server${i + 1} ansible_host=${pip.ip_address}"
+    ]
+  ))
+}
+
+#push to an inv with =======> terraform output ansible_inventory > inventory.ini
+#Public IP for RHEL VM
+resource "azurerm_public_ip" "rhel_public_ip" {
+  name                = "RHELPublicIP"
+  location            = azurerm_resource_group.brinek_rg.location
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+#Network Interface for RHEL VM
+resource "azurerm_network_interface" "rhel_nic" {
+  name                = "RHELNIC"
+  location            = azurerm_resource_group.brinek_rg.location
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.brinek_public_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.rhel_public_ip.id
+  }
+}
+
 
 # RHEL Linux VM
-# resource "azurerm_linux_virtual_machine" "rhel_vm" {
-#   name                = "RHELVM"
-#   resource_group_name = azurerm_resource_group.brinek_rg.name
-#   location            = azurerm_resource_group.brinek_rg.location
-#   size                = "Standard_D4s_v3"
-#   admin_username      = "brine"
-#   admin_password      = "Bravedemo123!"
-#   network_interface_ids = [
-#     azurerm_network_interface.rhel_nic.id,
-#   ]
+resource "azurerm_linux_virtual_machine" "rhel_vm" {
+  name                = "RHELVM"
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+  location            = azurerm_resource_group.brinek_rg.location
+  size                = "Standard_D4s_v3"
+  admin_username      = "brine"
+  admin_password      = "Bravedemo123!"
+  network_interface_ids = [
+    azurerm_network_interface.rhel_nic.id,
+  ]
 
-#   os_disk {
-#     caching              = "ReadWrite"
-#     disk_size_gb         = 127
-#     storage_account_type = "Standard_LRS"
-#   }
+  os_disk {
+    caching              = "ReadWrite"
+    disk_size_gb         = 127
+    storage_account_type = "Standard_LRS"
+  }
 
-#   source_image_reference {
-#     publisher = "RedHat"
-#     offer     = "RHEL"
-#     sku       = "8-LVM"
-#     version   = "latest"
-#   }
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "8-LVM"
+    version   = "latest"
+  }
 
-# Run sudo firewall-cmd --permanent --add-service=http
-# sudo firewall-cmd --reload
-# sudo firewall-cmd --list-all
+custom_data = base64encode(<<-EOF
+  #!/bin/bash
+  sudo yum update -y
+  sudo yum install httpd -y
+  sudo systemctl enable httpd
+  sudo systemctl start httpd
+EOF
+)
+  disable_password_authentication = true
 
-# custom_data = base64encode(<<-EOF
-#   #!/bin/bash
-#   sudo yum update -y
-#   sudo yum install httpd -y
-#   sudo systemctl enable httpd
-#   sudo systemctl start httpd
-# EOF
-# )
-#   disable_password_authentication = false
-
-#   tags = {
-#     Name = "RHELVM"
-#     Env  = "Development"
-#   }
-# }
-
-# Output Public IPs
-output "ubuntu_public_ip" {
-  value = azurerm_public_ip.ubuntu_public_ip.ip_address
+  admin_ssh_key {
+  username   = "brine"
+  public_key = file("/Users/brinketu/Downloads/eks-terraform-key.pub")
 }
 
-# output "rhel_public_ip" {
-#   value = azurerm_public_ip.rhel_public_ip.ip_address
-# }
+  tags = {
+    Name = "RHELVM"
+    Env  = "Development"
+  }
+}
 
-#SSH to RHEL VM: ssh brine@${azurerm_public_ip.rhel_public_ip.ip_address}
-output "ssh_instructions" {
-  value = <<EOF
-SSH to Ubuntu VM: ssh brine@${azurerm_public_ip.ubuntu_public_ip.ip_address}
-SSH to Ubuntu VM: ssh brine@${azurerm_public_ip.ubuntu_lb_pip.ip_address} -p 60022
+#Centos
+resource "azurerm_public_ip" "centos_public_ip" {
+  name                = "CentOSPublicIP"
+  location            = azurerm_resource_group.brinek_rg.location
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
 
+resource "azurerm_network_interface" "centos_nic" {
+  name                = "CentOSNIC"
+  location            = azurerm_resource_group.brinek_rg.location
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.brinek_public_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.centos_public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "centos_vm" {
+  name                = "CentOSVM"
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+  location            = azurerm_resource_group.brinek_rg.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "brine"
+  admin_password      = "Bravedemo123!"  # Only if you're using password auth
+
+  network_interface_ids = [
+    azurerm_network_interface.centos_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    disk_size_gb         = 127
+    storage_account_type = "Standard_LRS"
+  }
+
+admin_ssh_key {
+  username   = "brine"
+  public_key = file("/Users/brinketu/Downloads/eks-terraform-key.pub")
+}
+
+  source_image_reference {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7_9"
+    version   = "latest"
+  }
+
+disable_password_authentication = true
+
+  custom_data = base64encode(<<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install -y httpd
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+  EOF
+  )
+
+  tags = {
+    Name = "CentOSVM"
+    Env  = "Development"
+  }
+}
+
+# Windows VM
+# Public IP for Windows VM
+resource "azurerm_public_ip" "windows_public_ip" {
+  name                = "WindowsPublicIP"
+  location            = azurerm_resource_group.brinek_rg.location
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Network Interface for Windows VM
+resource "azurerm_network_interface" "windows_nic" {
+  name                = "WindowsNIC"
+  location            = azurerm_resource_group.brinek_rg.location
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.brinek_public_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.windows_public_ip.id
+  }
+}
+
+# Windows VM
+resource "azurerm_windows_virtual_machine" "windows_vm" {
+  name                = "WindowsVM"
+  resource_group_name = azurerm_resource_group.brinek_rg.name
+  location            = azurerm_resource_group.brinek_rg.location
+  size                = "Standard_D4s_v3"
+  admin_username      = "brineadmin"
+  admin_password      = "BraveDemoWin123!"  # Ensure this meets Azure password complexity requirements
+  depends_on = [azurerm_network_interface.windows_nic]
+
+  network_interface_ids = [
+    azurerm_network_interface.windows_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 127
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  provision_vm_agent        = true
+  enable_automatic_updates  = true
+
+custom_data = base64encode(<<-EOF
+<powershell>
+
+# Enable Remote Desktop
+Set-ItemProperty -Path 'HKLM:\\System\\CurrentControlSet\\Control\\Terminal Server' -Name "fDenyTSConnections" -Value 0
+
+# Enable RDP firewall rules (both pre-Windows 2012 and newer)
+Get-NetFirewallRule -DisplayGroup "Remote Desktop" | Enable-NetFirewallRule
+
+# Start the Remote Desktop service if not running
+if ((Get-Service TermService).Status -ne 'Running') {
+    Start-Service TermService
+}
+
+# Optional: Set Network Level Authentication to require it (recommended)
+Set-ItemProperty -Path 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\Terminal Server\\WinStations\\RDP-Tcp' -Name 'UserAuthentication' -Value 1
+
+</powershell>
 EOF
+)
+
+  tags = {
+    Name = "WindowsVM"
+    Env  = "Development"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "winrm_config" {
+  name                 = "winrm-config-extension"
+  virtual_machine_id   = azurerm_windows_virtual_machine.windows_vm.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.10"  # latest stable version
+
+  # Use 'settings' for non-sensitive public config only, but fileUris is sensitive - use protected_settings
+  settings = <<SETTINGS
+{}
+SETTINGS
+
+  protected_settings = <<PROTECTED_SETTINGS
+{
+  "fileUris": [
+    "https://raw.githubusercontent.com/ansible/ansible/stable-2.9/examples/scripts/ConfigureRemotingForAnsible.ps1"
+  ],
+  "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File ConfigureRemotingForAnsible.ps1"
+}
+PROTECTED_SETTINGS
+
+  depends_on = [azurerm_windows_virtual_machine.windows_vm]
+}
+# Output to get the RDP command
+output "rdp_command_to_windows" {
+  value = "mstsc /v:${azurerm_public_ip.windows_public_ip.ip_address}"
+}
+
+
+output "centos_public_ip" {
+  value = azurerm_public_ip.centos_public_ip.ip_address
+}
+output "centos_ssh_command" {
+  value = "ssh -i /path/to/your/private/key brine@${azurerm_public_ip.centos_public_ip.ip_address}"
+}
+output "ssh_command_to_rhel" {
+  value = "ssh -i /path/to/your/private/key brine@${azurerm_public_ip.rhel_public_ip.ip_address}"
 }
--- deploy.py
+++ deploy.py
@@ -1,21 +1,32 @@
-def reverse_words(sentence):
-    result = ""
-    word = "" 
-    for char in sentence:
-        if char != " ":
-            word += char
-        else:
-            result += word[::-1] + " "
-            word   = ""
+import pandas as pd
+from io import StringIO
 
-    result += word[::-1]
-    return result
+# Simulate reading from CSV
+csv_data = """year,month,day,hour,seconds,transaction_id,amount
+2025,1,15,15,59,AA,1
+2025,2,14,23,55,B,2
+2021,10,13,22,55,AA,1
+2023,11,12,8,50,AA,2
+2025,3,11,7,45,AA,1
+1990,4,10,2,45,B,2
+2001,4,9,11,45,B,1
+1990,4,10,2,45,B,2
+2021,10,13,22,55,AA,1"""
 
+# Load into DataFrame
+df = pd.read_csv(StringIO(csv_data))
 
-input_sentence = "this is a test"
-output_sentence = reverse_words(input_sentence)
+# Step 1: Standardize date format
+df['date'] = pd.to_datetime(df[['year', 'month', 'day', 'hour']].assign(minute=0, second=df['seconds']))
 
-print(output_sentence)
+# Step 2: Remove duplicates
+df = df.drop_duplicates()
 
+# Step 3: Aggregate on transaction_id
+aggregated = df.groupby('transaction_id', as_index=False).agg({
+    'date': 'min',  # earliest transaction date
+    'amount': 'sum'  # total amount
+})
 
-
+# Final output
+print(aggregated)
